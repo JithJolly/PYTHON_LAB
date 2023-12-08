9bec97e23ad33d4bc85fb74ead4bed4c3ac2512d
Jith Jolly <145551249+JithJolly@users.noreply.github.com>
2023-12-08 16:09:21 +0530

    Delete read AND write directory

--- read AND write/2.py
+++
@@ -1,4 +0,0 @@
-f=open("2.txt","w")
-f.write("My first File")
-f.close()
-print("File",f.name,"Closed")
--- read AND write/3.py
+++
@@ -1,4 +0,0 @@
-f=open("3.txt","w")
-f.write("Jith Jolly")
-f.close()
-print("File",f.name,"Closed")
